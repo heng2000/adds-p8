0ed7bb467b0a64ad6501f9894090d68d308746a5
a <liyuhengduigong@gmail.com>
2023-05-22 22:37:58 +0930

    li

--- Heap-skeleton.cpp
+++ Heap-skeleton.cpp
@@ -1,51 +1,61 @@
-/*
- * @Author: yuheng li a1793138
- * @Date: 2023-05-22 19:06:30
- * @LastEditors: yuheng 
- * @LastEditTime: 2023-05-22 22:35:41
- * @FilePath: \adds-p8\Heap-skeleton.cpp
- * @Description: 
- * 
- * Copyright (c) ${2023} by ${yuheng li}, All Rights Reserved. 
- */
-#ifndef HEAP_H_
-#define HEAP_H_
-#include<vector>
-#include<cmath>
+#ifndef HEAP_H
+#define HEAP_H
+
+#include <vector>
+#include <cmath> // for floor()
 
 template <typename T>
-class Heap
-{
-    private:
-    //values numbers
-        std::vector<T> numbers;
-        void heapify(int);
-    public:
-        Heap( );
-        Heap(std::vector<T>);
-        void insert(T value);
-        void remove(T value):
-        T getMin();
+class Heap {
+ private:
+  std::vector<T> values;
+  void heapify(int);
+
+ public:
+  Heap();  // constructs an empty heap
+
+  // constructs a heap with the values from the given vector
+  // the type T MUST provide comparison operators <, >, ==
+  Heap(std::vector<T>);
+
+  void insert(T);
+  void remove(T);
+  T getMin();
 };
+
+/*******************************/
+// default constructor
+/*******************************/
 
 template <typename T>
 Heap<T>::Heap() {}
+
+/*******************************/
+// constructs a heap with the values in the given vector
+// the type T MUST provide comparison operators <, >, ==
+/*******************************/
+
 template <typename T>
-Heap<T>::Heap(std::vector<T> start_values) 
-{
-    for (int i = 0; i< start_values.size(); i++)
-    {
-        numbers.push_back(start_values.at(i));
-    }
-    int initial_parent_index = floor(numbers.size() / 2) - 1;
-    for(int parent_index = initial_parent_index; parent_index >= 0;parent_index--)
-    {
-        heapify(parent_index);
-    }
+Heap<T>::Heap(std::vector<T> start_values) {
+  // copy the values into our heap vector
+  for (int i = 0; i < start_values.size(); i++) {
+    values.push_back(start_values.at(i));
+  }
+
+  // starting from last non-leaf node (last parent), heapify each
+  // of the parents
+  int initial_parent_index = floor((values.size()-1) / 2);
+  for (int parent_index = initial_parent_index; parent_index >= 0;
+       parent_index--) {
+    heapify(parent_index);
+  }
 }
+
+/*******************************/
+// add values to the heap
+/*******************************/
+
 template <typename T>
-void Heap<T>::insert(T value)
-{
+void Heap<T>::insert(T value) {
     numbers.push_back(value);
     int child_index =numbers.size()-1;
     int parent_index = (numbers.size()-1)/2;
@@ -58,9 +68,13 @@ void Heap<T>::insert(T value)
         parent_index = (child_index - 1) / 2;
     }
 }
+
+/*******************************/
+/* delete values from the heap */
+/*******************************/
+
 template <typename T>
-void Heap<T>::remove(T value)
-{
+void Heap<T>::remove(T value) {
     int index = -1;
     for(int i=0; i<values.size(); i++)
     {
@@ -74,11 +88,57 @@ void Heap<T>::remove(T value)
         numbers.pop_back();
         heapify(index);
     }
-
 }
+
+/*******************************/
+// find the smallest value in the heap
+/*******************************/
+
 template <typename T>
-T Heap<T>::getMin() 
-{
+T Heap<T>::getMin() {
     return numbers[0];
 }
+
+/*******************************/
+// private function to heapify a given 'node'
+/*******************************/
+
+template <typename T>
+void Heap<T>::heapify(int parent_index) {
+  // if we're outside the index range, return
+  if (parent_index < 0 || parent_index >= values.size()) return;
+
+  // find children indexes
+  int left_child_index = parent_index * 2 + 1;
+  int right_child_index = parent_index * 2 + 2;
+
+  // if parent is larger than child, swap with smallest child
+  int index_of_smallest = parent_index;
+
+  // check if left child exists and if exists, is smallest value there
+  if (left_child_index < values.size() &&
+      values.at(left_child_index) < values.at(index_of_smallest)) {
+    // make this index the current smallest
+    index_of_smallest = left_child_index;
+  }
+
+  // check if left child exists and if exists, is smallest value there
+  if (right_child_index < values.size() &&
+      values.at(right_child_index) < values.at(index_of_smallest)) {
+    // make this index the current smallest
+    index_of_smallest = right_child_index;
+  }
+
+  // if parent is not smallest, swap with smallest child
+  if (index_of_smallest != parent_index) {
+    T temp = values.at(parent_index);
+    values.at(parent_index) = values.at(index_of_smallest);
+    values.at(index_of_smallest) = temp;
+
+    // heapify the swapped index - it may need to move
+    // further down the 'tree'
+    heapify(index_of_smallest);
+  }
+}
+
 #endif
